d3d8bb135f7c1193e81cff41e8b2ba7ff86cb0b5
A-Melnykk <969696245n@gmail.com>
2026-06-02 13:16:43 +0300

    add task solution

---
+++ src/Autocomplete.tsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useState, useRef } from 'react';
+import { Person } from './types/Person';
+
+type Props = {
+  people: Person[];
+  delay?: number;
+  onSelected: (person: Person) => void;
+  onQueryChange: () => void;
+};
+
+export const Autocomplete: React.FC<Props> = ({
+  people,
+  delay = 300,
+  onSelected,
+  onQueryChange,
+}) => {
+  const [query, setQuery] = useState('');
+  const [visiblePeople, setVisiblePeople] = useState<Person[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const lastQuery = useRef('');
+
+  useEffect(() => {
+    if (query === lastQuery.current) {
+      return;
+    }
+
+    const timer = setTimeout(() => {
+      const filtered = people.filter(person =>
+        person.name.toLowerCase().includes(query.toLowerCase()),
+      );
+
+      setVisiblePeople(filtered);
+      lastQuery.current = query;
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, [query, people, delay]);
+
+  const handleChange = (value: string) => {
+    setQuery(value);
+    setIsOpen(true);
+    onQueryChange();
+  };
+
+  return (
+    <>
+      <div className={`dropdown ${isOpen ? 'is-active' : ''}`}>
+        <div className="dropdown-trigger">
+          <input
+            type="text"
+            className="input"
+            placeholder="Enter a part of the name"
+            value={query}
+            data-cy="search-input"
+            onFocus={() => {
+              setIsOpen(true);
+              if (!query) {
+                setVisiblePeople(people);
+              }
+            }}
+            onChange={e => handleChange(e.target.value)}
+          />
+        </div>
+
+        {isOpen && (
+          <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
+            <div className="dropdown-content">
+              {visiblePeople.length > 0 ? (
+                visiblePeople.map(person => (
+                  <div
+                    key={person.slug}
+                    className="dropdown-item"
+                    data-cy="suggestion-item"
+                    onClick={() => {
+                      setQuery(person.name);
+                      setIsOpen(false);
+                      onSelected(person);
+                    }}
+                  >
+                    <p className="has-text-link">{person.name}</p>
+                  </div>
+                ))
+              ) : (
+                <div className="dropdown-item" data-cy="no-suggestions-message">
+                  <p className="has-text-danger">No matching suggestions</p>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+      </div>
+
+      {isOpen && visiblePeople.length === 0 && (
+        <div
+          className="
+    notification
+    is-danger
+    is-light
+    mt-3
+    is-align-self-flex-start
+  "
+          role="alert"
+          data-cy="no-suggestions-message"
+        >
+          <p className="has-text-danger">No matching suggestions</p>
+        </div>
+      )}
+    </>
+  );
+};
